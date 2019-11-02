2849c25a5894c5dac3b6420a09cded02db46a291
Ravshan Samandarov <ravshansbox@gmail.com>
2019-11-02 19:42:36 +0500

    Add normalizeUrlQuery function. Closes #7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { appFactory } from './app-factory';
+export { asyncHandler } from './async-handler';
+export { normalizeUrlQuery } from './normalize-url-query';
 export { parseBody } from './parse-body';
 export { IMap, IContext, IHandler, IMatch, IMiddleware, INext } from './types';
-export { asyncHandler } from './async-handler';
