091a5b0b7547fc4f4e0f3e52ee2271f0d451432b
Ravshan Samandarov <ravshansbox@gmail.com>
2020-08-04 08:15:24 +0500

    feat: Export rpcHandlerFactory

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { appFactory } from './app-factory';
 export { asyncHandler } from './async-handler';
 export { normalizeUrlQuery } from './normalize-url-query';
 export { parseBody } from './parse-body';
+export { rpcHandlerFactory } from './rpc-handler-factory';
 export { Context, Handler, Hash, Match, Middleware, Next } from './types';
