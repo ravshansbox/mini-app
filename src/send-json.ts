e6a663019a915efe3bcfce0f044e5742e2b79b92
Ravshan Samandarov <rsamandarov@opera.com>
2023-02-25 16:42:17 +0300

    fix `sendJson()` status code response

--- src/send-json.ts
+++ src/send-json.ts
@@ -2,7 +2,7 @@ import { type ServerResponse } from 'http';
 import { CONTENT_TYPES, HEADERS } from './types';
 
 export const sendJson = (response: ServerResponse, payload: any, statusCode: number) => {
+  response.statusCode = statusCode;
   response.setHeader(HEADERS.CONTENT_TYPE, CONTENT_TYPES.JSON);
   response.end(JSON.stringify(payload));
-  response.statusCode = statusCode;
 };
