071a2c93eecf58afe882b4351909a708343a0b68
Ravshan Samandarov <ravshansbox@gmail.com>
2023-11-18 23:09:50 +0300

    use method shothands syntax for router object

--- src/createRouter.ts
+++ src/createRouter.ts
@@ -11,27 +11,27 @@ export const createRouter = () => {
 
   return {
     routes: routesWithMatch,
-    addRoute: (route: Route) => {
+    addRoute(route: Route) {
       addRoute(route);
       return this;
     },
-    addRoutes: (basePath: string, routes: Route[]) => {
+    addRoutes(basePath: string, routes: Route[]) {
       routes.map((route) => ({ ...route, path: `${basePath}${route.path}` })).forEach(addRoute);
       return this;
     },
-    get: (path: string, handler: Handler) => {
+    get(path: string, handler: Handler) {
       addRoute({ method: 'GET', path, handler });
       return this;
     },
-    post: (path: string, handler: Handler) => {
+    post(path: string, handler: Handler) {
       addRoute({ method: 'POST', path, handler });
       return this;
     },
-    put: (path: string, handler: Handler) => {
+    put(path: string, handler: Handler) {
       addRoute({ method: 'PUT', path, handler });
       return this;
     },
-    delete: (path: string, handler: Handler) => {
+    delete(path: string, handler: Handler) {
       addRoute({ method: 'DELETE', path, handler });
       return this;
     },
